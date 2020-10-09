8810a349c44999a3000db9c7df6b02dda72b839c
huangyangcong <1352640897@qq.com>
2020-10-09 11:58:11 +0800

    fixed

--- simple4j-flow-demo/src/main/java/com/simple4j/flow/FlowDemoApplication.java
+++ simple4j-flow-demo/src/main/java/com/simple4j/flow/FlowDemoApplication.java
@@ -1,13 +1,44 @@
 package com.simple4j.flow;
 
 
+import com.google.common.collect.Lists;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FlowDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlowDemoApplication.class, args);
+	}
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource(){
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Lists.newArrayList(CorsConfiguration.ALL));
+		configuration.setAllowedMethods(Lists.newArrayList(CorsConfiguration.ALL));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**",configuration);
+		return source;
+	}
+	@Configuration
+	public class WebMvcConfig implements WebMvcConfigurer {
+
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+					.allowedOrigins("*")
+					.allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+					.maxAge(3600)
+					.allowCredentials(true);
+		}
 	}
 }
