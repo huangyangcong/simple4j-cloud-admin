a151fd96ad5f8a5e51ce63d8c7e02d964818ce1f
1352640897@qq.com <1352640897@qq.com>
2020-10-22 08:57:21 +0800

    fixed

---
+++ simple4j-flow/flow-web/src/main/java/com/simple4j/flow/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.simple4j.flow.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+				.anyRequest().permitAll().and().logout().permitAll();//配置不需要登录验证
+	}
+}
