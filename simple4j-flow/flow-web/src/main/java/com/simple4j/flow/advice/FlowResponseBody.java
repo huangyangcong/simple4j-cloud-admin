8d5dd4f9c93614de06ba742b545cfeea6b8bf195
huangyangcong <huangyangcong@chainfly.com>
2020-10-22 18:36:41 +0800

    fixed

--- simple4j-flow/flow-web/src/main/java/com/simple4j/flow/advice/FlowResponseBody.java
+++ simple4j-flow/flow-web/src/main/java/com/simple4j/flow/advice/FlowResponseBody.java
@@ -3,6 +3,7 @@ package com.simple4j.flow.advice;
 import com.simple4j.web.bean.ApiResponse;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
@@ -13,11 +14,12 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 public class FlowResponseBody implements ResponseBodyAdvice<Object> {
 	@Override
 	public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
-		return true;
+		return converterType != ByteArrayHttpMessageConverter.class;
 	}
 
 	@Override
 	public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+
 		return ApiResponse.ok(body);
 	}
 }
--- simple4j-flow/flow-web/src/main/java/com/simple4j/flow/config/SecurityConfig.java
+++ simple4j-flow/flow-web/src/main/java/com/simple4j/flow/config/SecurityConfig.java
@@ -13,6 +13,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests()
-				.anyRequest().permitAll().and().logout().permitAll();//配置不需要登录验证
+				.anyRequest().permitAll()
+				.and()
+				.logout().permitAll()//配置不需要登录验证
+				.and()
+				.csrf().disable();
 	}
 }
