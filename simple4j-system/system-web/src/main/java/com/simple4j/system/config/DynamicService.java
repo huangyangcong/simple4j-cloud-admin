bcb8183fa8130a14e5c5b8c3c4f6f8bfdba84789
1352640897@qq.com <1352640897@qq.com>
2020-10-29 13:23:25 +0800

    fixed

--- simple4j-system/system-web/src/main/java/com/simple4j/system/config/DynamicService.java
+++ simple4j-system/system-web/src/main/java/com/simple4j/system/config/DynamicService.java
@@ -7,6 +7,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractConfigAttributeRequestMatcherRegistry;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.access.expression.ExpressionBasedFilterInvocationSecurityMetadataSource;
@@ -36,7 +37,8 @@ public class DynamicService implements DynamicSecurityService, ApplicationContex
 				new ExpressionUrlAuthorizationConfigurer<>(context);
 		ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry = hExpressionUrlAuthorizationConfigurer.getRegistry();
 		registry.mvcMatchers(HttpMethod.DELETE, "/user/api/v1/info").authenticated();
-		Method createRequestMap = ClassUtils.getMethod(ExpressionUrlAuthorizationConfigurer.ExpressionInterceptUrlRegistry.class, "createRequestMap");
+		Method createRequestMap = ClassUtils.getMethod(AbstractConfigAttributeRequestMatcherRegistry.class, "createRequestMap");
+		createRequestMap.setAccessible(true);
 		LinkedHashMap<RequestMatcher, Collection<ConfigAttribute>> requestMap = (LinkedHashMap<RequestMatcher, Collection<ConfigAttribute>>) ReflectionUtils.invokeMethod(createRequestMap, registry);
 		return new ExpressionBasedFilterInvocationSecurityMetadataSource(requestMap, new DefaultWebSecurityExpressionHandler());
 	}
