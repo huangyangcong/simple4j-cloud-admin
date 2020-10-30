3376394436801e23406e07cebc1817515be1f73a
huangyangcong <huangyangcong@chainfly.com>
2020-10-30 09:00:30 +0800

    fixed

---
+++ simple4j-system/system-web/src/main/java/com/simple4j/system/config/DynamicSecurityServiceImpl.java
@@ -0,0 +1,26 @@
+package com.simple4j.system.config;
+
+import com.simple4j.autoconfigure.jwt.dynamic.DynamicSecurityService;
+import com.simple4j.autoconfigure.jwt.dynamic.IgnoreAbstractRequestMatcherRegistry;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author hyc
+ */
+@Component
+public class DynamicSecurityServiceImpl implements DynamicSecurityService {
+
+
+	@Override
+	public void loadDataSource(ExpressionUrlAuthorizationConfigurer.ExpressionInterceptUrlRegistry registry, DefaultWebSecurityExpressionHandler expressionHandler) {
+		registry.mvcMatchers(HttpMethod.POST, "/user/api/v1/info").authenticated();
+	}
+
+	@Override
+	public void ignoreUrls(IgnoreAbstractRequestMatcherRegistry ignoreAbstractRequestMatcherRegistry) {
+	}
+}
