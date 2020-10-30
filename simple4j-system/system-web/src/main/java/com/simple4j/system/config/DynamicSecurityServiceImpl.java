b247cd2df31965ddb4e6523eccaf28ffe0d82601
huangyangcong <huangyangcong@chainfly.com>
2020-10-30 11:35:04 +0800

    fixed

--- simple4j-system/system-web/src/main/java/com/simple4j/system/config/DynamicSecurityServiceImpl.java
+++ simple4j-system/system-web/src/main/java/com/simple4j/system/config/DynamicSecurityServiceImpl.java
@@ -4,6 +4,7 @@ import com.simple4j.autoconfigure.jwt.dynamic.DynamicSecurityService;
 import com.simple4j.autoconfigure.jwt.dynamic.IgnoreAbstractRequestMatcherRegistry;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.stereotype.Component;
@@ -16,11 +17,13 @@ public class DynamicSecurityServiceImpl implements DynamicSecurityService {
 
 
 	@Override
-	public void loadDataSource(ExpressionUrlAuthorizationConfigurer.ExpressionInterceptUrlRegistry registry, DefaultWebSecurityExpressionHandler expressionHandler) {
-		registry.mvcMatchers(HttpMethod.POST, "/user/api/v1/info").authenticated();
+	public void loadDataSource(ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry,
+							   DefaultWebSecurityExpressionHandler expressionHandler) {
+		registry.mvcMatchers(HttpMethod.POST, "/user/api/v1/info").permitAll().anyRequest().authenticated();
 	}
 
 	@Override
 	public void ignoreUrls(IgnoreAbstractRequestMatcherRegistry ignoreAbstractRequestMatcherRegistry) {
+		ignoreAbstractRequestMatcherRegistry.mvcMatchers("/aaaa");
 	}
 }
