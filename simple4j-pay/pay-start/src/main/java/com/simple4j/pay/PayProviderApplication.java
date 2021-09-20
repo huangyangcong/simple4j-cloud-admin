6b17fb346c5220382ca4e43f9ced2b93bfd74d81
huangyangcong <1352640897@qq.com>
2021-09-20 13:32:33 +0800

    优化支付模块

--- simple4j-pay/pay-start/src/main/java/com/simple4j/pay/PayProviderApplication.java
+++ simple4j-pay/pay-start/src/main/java/com/simple4j/pay/PayProviderApplication.java
@@ -1,9 +1,7 @@
 package com.simple4j.pay;
 
-import com.simple4j.pay.config.PayConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * @author hyc
